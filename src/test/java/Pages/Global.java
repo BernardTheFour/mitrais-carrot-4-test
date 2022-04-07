8e7da009658b27b3d1feab68437dd8786a2a1dcc
BernardTheFour <bernardctxy1@gmail.com>
2022-04-07 12:55:20 +0700

    -->Change: global farmer homepage url

--- src/test/java/Pages/Global.java
+++ src/test/java/Pages/Global.java
@@ -1,7 +1,9 @@
 package Pages;
 
 public class Global {
-    public static String WebURL = "http://localhost:3000/";
+    public static String WebURL = "http://localhost:3000";
+    public static String FarmerHomePage = "/home/farmer";
+
     public static String chromeDriverName ="webdriver.chrome.driver";
     public static String chromeDriverPath ="src/test/webdriver/chromedriver_v100.exe";
 
