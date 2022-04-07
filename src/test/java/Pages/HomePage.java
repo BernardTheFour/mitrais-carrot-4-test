58d7a7cfd119af915d6cbf86576e02467673eb39
BernardTheFour <bernardctxy1@gmail.com>
2022-04-07 12:36:37 +0700

    -->DONE: farmer can login

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,5 +1,55 @@
 package Pages;
 
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
-    
+    protected WebDriver driver;
+
+    // Locators for navbar (every page have navbar)
+    By userDescPath = By.xpath("//div[@class='dropdown-menu show dropdown-menu-end']//div//div[1]");
+    By profileDropdownPath = By.xpath("//a[@id='basic-nav-dropdown']");
+
+    By fullnamePath(String fullname) {
+        return By.xpath("//h3[normalize-space()='" + fullname + "']");
+    }
+
+    // Elements
+    protected WebElement fullnameElement, userDescElement, profileDropdownElement;
+
+    protected void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected void assignPageElement(String fullname) {        
+        profileDropdownElement = driver.findElement(profileDropdownPath);
+        profileDropdownElement.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(fullnamePath(fullname)));
+        fullnameElement = driver.findElement(fullnamePath(fullname));
+        userDescElement = driver.findElement(userDescPath);
+    }
+
+    public void assertUser(
+            String expectedFullname,
+            String expectedGrade,
+            String expectedPosition,
+            String expectedRole) {
+
+        // ex: "StaffJunior Programmer, Training and DevelopmentStaff"
+        String userDesc = expectedFullname 
+                + expectedGrade + ", "
+                + expectedPosition
+                + expectedRole;
+
+        Assert.assertEquals(expectedFullname, fullnameElement.getText());
+        Assert.assertEquals(userDesc, userDescElement.getText().replace("\n", "")); // delete newline
+    }
 }
