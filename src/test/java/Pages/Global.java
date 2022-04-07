b3327a9b580d03ce0643bca10e25fe248f6e7592
BernardTheFour <bernardctxy1@gmail.com>
2022-04-07 12:57:35 +0700

    -->RENAME: FarmerHomePage -> FarmerHomeURL

--- src/test/java/Pages/Global.java
+++ src/test/java/Pages/Global.java
@@ -1,9 +1,11 @@
 package Pages;
 
 public class Global {
+    // page url
     public static String WebURL = "http://localhost:3000";
-    public static String FarmerHomePage = "/home/farmer";
+    public static String FarmerHomeURL = "/home/farmer";
 
+    // browser driver
     public static String chromeDriverName ="webdriver.chrome.driver";
     public static String chromeDriverPath ="src/test/webdriver/chromedriver_v100.exe";
 
