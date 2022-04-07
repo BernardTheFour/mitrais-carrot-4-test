bcaf9b2f1ecd7c899f1157c2b4cc59937423e227
BernardTheFour <bernardctxy1@gmail.com>
2022-04-07 11:03:33 +0700

    --> Fixed: login test

--- src/test/java/Test/TestLogin.java
+++ src/test/java/Test/TestLogin.java
@@ -1,16 +1,19 @@
 package Test;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import Pages.HomePage;
 import Pages.LoginPage;
 
 public class TestLogin {
     static WebDriver driver;
     static LoginPage loginPage;
+    static HomePage homePage;
 
     @BeforeClass
     public static void beforeLogin(){        
@@ -23,12 +26,21 @@ public class TestLogin {
         loginPage.login("user_farmer", "1234");
     }
 
-    //After all tests
-    //@AfterClass
-    public static void closeBrowser(){
+    @Test
+    public void loginFail(){
+        loginPage.login("wronguser", "wrongpassword");
+        loginPage.assertErrorMessage();
+    }
+    
+    @After
+    public void clearCache(){
         //Delete cookies to logout user
         driver.manage().deleteAllCookies();
-        
+    }
+
+    //After all tests
+    @AfterClass
+    public static void closeBrowser(){        
         //Terminate the WebDriver
         driver.quit();
     }
