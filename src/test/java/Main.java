e9b7fbb84b30aa9fe1fb88935ca6ecd723ba525b
BernardTheFour <bernardctxy1@gmail.com>
2022-04-07 08:39:30 +0700

    Initial push

---
+++ src/test/java/Main.java
@@ -0,0 +1,9 @@
+import org.junit.runner.Result;
+
+public class Main {
+    //private static Result result;
+
+    public static void main (String[] args){
+        System.out.println("RUN");
+    }    
+}
