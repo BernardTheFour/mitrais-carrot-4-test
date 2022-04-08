cfa86816028aa3a6b5d54d0fae1573d0c5256635
Hafid Sriwijaya <32842793+hafidsb@users.noreply.github.com>
2022-04-08 15:00:31 +0700

    automate test for M-2.1
    Merchant cannot create Bazaar Item if missing mandatory field

--- src/test/java/Tab/MerchandiseTab.java
+++ src/test/java/Tab/MerchandiseTab.java
@@ -3,6 +3,7 @@ package Tab;
 import Dto.BazaarItem;
 import Pages.Global;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -19,6 +20,7 @@ public class MerchandiseTab implements IHompageTab {
     By merchTabPath = By.xpath("//h3[normalize-space()='Merchandise']");
     By createBtnPath = By.xpath("//button[normalize-space()='Add Item']");
     By submitBtnPath = By.xpath("//button[normalize-space()='Submit']");
+    By createErrMsgPath = By.xpath("//div[@role='alert']");
 
     // Create Item Form Locators
     By itemNamePath = By.xpath("//input[@name='ItemName']");
@@ -26,7 +28,6 @@ public class MerchandiseTab implements IHompageTab {
     By itemImgPath = By.xpath("//input[@name='ItemImg']");
     By itemPricePath = By.xpath("//input[@name='ItemPrice']");
     By itemStockPath = By.xpath("//input[@name='ItemStock']");
-    By itemBazaarPath = By.xpath("//input[@name='BazaarId']");
     By itemStartDatePath = By.xpath("//input[@name='ItemStartDate']");
     By itemEndDatePath = By.xpath("//input[@name='ItemEndDate']");
 
@@ -54,6 +55,11 @@ public class MerchandiseTab implements IHompageTab {
         driver.findElement(submitBtnPath).click();
     }
 
+    public void createEmptyItem() {
+        driver.findElement(createBtnPath).click();
+        driver.findElement(submitBtnPath).click();
+    }
+
     public BazaarItem getLastItem() {
         BazaarItem newItem = new BazaarItem();
         WebElement merchTblLastPageBtnElement = driver.findElement(merchTblLastPageBtnPath);
@@ -74,13 +80,19 @@ public class MerchandiseTab implements IHompageTab {
         return newItem;
     }
 
+    public void assertErrorMessage(String expectedErrorMsg) {
+        new WebDriverWait(driver, Global.Timeout)
+                .until(ExpectedConditions.visibilityOfElementLocated(createErrMsgPath));
+
+        Assertions.assertEquals(expectedErrorMsg, driver.findElement(createErrMsgPath).getText());
+    }
+
     private void fillCreateForm(BazaarItem item) {
         driver.findElement(itemNamePath).sendKeys(item.getName());
         driver.findElement(itemDescPath).sendKeys(item.getDescription());
         driver.findElement(itemImgPath).sendKeys(item.getImage());
         driver.findElement(itemPricePath).sendKeys(item.getPrice());
         driver.findElement(itemStockPath).sendKeys(item.getStock());
-        driver.findElement(itemBazaarPath).sendKeys(item.getBazaarId());
         driver.findElement(itemStartDatePath).sendKeys(item.getStartDate());
         driver.findElement(itemEndDatePath).sendKeys(item.getEndDate());
     }
