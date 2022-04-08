d00a2b0fa9bffe26d85112144458adb4461ea896
Hafid Sriwijaya <32842793+hafidsb@users.noreply.github.com>
2022-04-08 08:41:04 +0700

    fix assertion table for row>10

--- src/test/java/Pages/Merchant.java
+++ src/test/java/Pages/Merchant.java
@@ -2,16 +2,20 @@ package Pages;
 
 import Dto.BazaarItem;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Merchant extends HomePage {
 
     // Locators
     By merchTabPath = By.xpath("//h3[normalize-space()='Merchandise']");
+    By transTabPath = By.xpath("//h3[normalize-space()='Transaction']");
     By createBtnPath = By.xpath("//button[normalize-space()='Add Item']");
     By submitBtnPath = By.xpath("//button[normalize-space()='Submit']");    
     
@@ -29,40 +33,53 @@ public class Merchant extends HomePage {
     By lastTblItemPricePath = By.xpath("(//div[@role='table'])[1]/div[2]/div[last()]/div[@data-column-id='2']/div");
     By lastTblItemStockPath = By.xpath("(//div[@role='table'])[1]/div[2]/div[last()]/div[@data-column-id='3']/div");
 
+    // Last Merch Table Page Locators
+    By merchTblLastPageBtnPath = By.xpath("(//*[@id=\"pagination-last-page\"])[1]");
+
     public Merchant(WebDriver driver) {
         super.driver = driver;
     }
 
     private void fillCreateForm(BazaarItem item) {
-        super.driver.findElement(itemNamePath).sendKeys(item.getName());
-        super.driver.findElement(itemDescPath).sendKeys(item.getDescription());
-        super.driver.findElement(itemImgPath).sendKeys(item.getImage());
-        super.driver.findElement(itemPricePath).sendKeys(item.getPrice());
-        super.driver.findElement(itemStockPath).sendKeys(item.getStock());
-        super.driver.findElement(itemBazaarPath).sendKeys(item.getBazaarId());
-        super.driver.findElement(itemStartDatePath).sendKeys(item.getStartDate());
-        super.driver.findElement(itemEndDatePath).sendKeys(item.getEndDate());
+        driver.findElement(itemNamePath).sendKeys(item.getName());
+        driver.findElement(itemDescPath).sendKeys(item.getDescription());
+        driver.findElement(itemImgPath).sendKeys(item.getImage());
+        driver.findElement(itemPricePath).sendKeys(item.getPrice());
+        driver.findElement(itemStockPath).sendKeys(item.getStock());
+        driver.findElement(itemBazaarPath).sendKeys(item.getBazaarId());
+        driver.findElement(itemStartDatePath).sendKeys(item.getStartDate());
+        driver.findElement(itemEndDatePath).sendKeys(item.getEndDate());
     }
 
     public void goToMerchTab() {
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(merchTabPath));
 
-        super.driver.findElement(merchTabPath).click();
+        driver.findElement(merchTabPath).click();
     }
 
     public void createItem(BazaarItem item) {
-        super.driver.findElement(createBtnPath).click();
+        driver.findElement(createBtnPath).click();
         fillCreateForm(item);
-        super.driver.findElement(submitBtnPath).click();
+        driver.findElement(submitBtnPath).click();
     }
 
     public BazaarItem getLastItem() {
         BazaarItem newItem = new BazaarItem();
+        WebElement merchTblLastPageBtnElement = driver.findElement(merchTblLastPageBtnPath);
+        int tableRow =  driver.findElements(By.xpath("(//div[@role='table'])[1]/div[2]/*")).size();
+        if (tableRow > 10) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView(true);", merchTblLastPageBtnElement);
+            merchTblLastPageBtnElement.click();
+        }
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(lastTblItemNamePath));
         newItem.setName(driver.findElement(lastTblItemNamePath).getText());
         newItem.setPrice(driver.findElement(lastTblItemPricePath).getText());
         newItem.setStock(driver.findElement(lastTblItemStockPath).getText());
-        System.out.println(newItem);
+
         return newItem;
     }
 }
