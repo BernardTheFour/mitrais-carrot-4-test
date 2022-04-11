bf022a9824d5969737e6a9ece8932f555d6afe0f
Alvian Nur Ramadhani <alviannur.ramadhani@mitrais.com>
2022-04-11 08:09:51 +0700

    add test manager cannot buy item

--- src/test/java/Pages/Manager.java
+++ src/test/java/Pages/Manager.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import Tab.BazaarTab;
 import Tab.CarrotDashBoard;
 import Tab.ShareCarrotTab;
 import org.openqa.selenium.WebDriver;
@@ -8,13 +9,16 @@ import org.openqa.selenium.WebDriver;
 public class Manager extends HomePage {
     private ShareCarrotTab shareCarrotTab;
     private CarrotDashBoard carrotDashboard;
+    private BazaarTab bazaarTab;
 
     public ShareCarrotTab shareCarrotTab(){return shareCarrotTab;}
     public CarrotDashBoard carroDashBoard(){return carrotDashboard;}
+    public BazaarTab bazaarTab(){return bazaarTab;}
 
     public Manager(WebDriver driver){
         super.driver=driver;
         shareCarrotTab = new ShareCarrotTab(driver);
         carrotDashboard = new CarrotDashBoard(driver);
+        bazaarTab = new BazaarTab(driver);
     }
 }
